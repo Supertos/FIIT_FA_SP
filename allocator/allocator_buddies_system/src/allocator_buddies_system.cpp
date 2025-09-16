e12d4c4b80cda4b993f2689cec7915c322aec141
Supertos <57270422+Supertos@users.noreply.github.com>
2025-09-16 23:50:14 +0300

    Add files via upload

--- allocator/allocator_buddies_system/src/allocator_buddies_system.cpp
+++ allocator/allocator_buddies_system/src/allocator_buddies_system.cpp
@@ -1,21 +1,38 @@
 #include <not_implemented.h>
 #include <cstddef>
+#include <cstring>
+#include <iostream>
 #include "../include/allocator_buddies_system.h"
 
 
-struct BuddyAllocatorBlockMetadata* allocator_buddies_system::next_block( struct BuddyAllocatorBlockMetadata* b ) const {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+allocator_buddies_system::BuddyBlock* allocator_buddies_system::next_block( allocator_buddies_system::BuddyBlock* b ) const {
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
 	
-	auto addr = reinterpret_cast<struct BuddyAllocatorBlockMetadata*>(reinterpret_cast<std::byte*>(b) + (size_t)pow(2, b->size)); // + sizeof(struct BuddyAllocatorBlockMetadata));
-	return reinterpret_cast<std::byte*>(addr) < reinterpret_cast<std::byte*>(data) + data->memSize - 2 ? addr : nullptr; // 2 Because of block metadata
+	auto addr = (allocator_buddies_system::BuddyBlock*)((uintptr_t)b + (size_t)pow(2, b->size)); // + sizeof(allocator_buddies_system::BuddyBlock));
+	return (uintptr_t)(addr) < (uintptr_t)data + data->memSize - 2 ? addr : nullptr; // 2 Because of block metadata
 }
+
+allocator_buddies_system::BuddyBlock* allocator_buddies_system::get_buddy( allocator_buddies_system::BuddyBlock* b ) const {
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
+    size_t block_size = size_t(1) << b->size;
+    uintptr_t base_address = ((uintptr_t)this->_trusted_memory + sizeof(BuddyMetadata));
+    uintptr_t block_offset = reinterpret_cast<uintptr_t>(b) - base_address;
+    uintptr_t buddy_offset = block_offset ^ block_size;
+    
+    if (buddy_offset + block_size > data->memSize) {
+        return nullptr;
+    }
+    
+    return reinterpret_cast<allocator_buddies_system::BuddyBlock*>(base_address + buddy_offset);
+}
+
 std::mutex& allocator_buddies_system::mutex() const {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
     return data->globalLock;
 }
 allocator_buddies_system::~allocator_buddies_system() {
 	std::lock_guard lock(this->mutex());
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
 	
 	if( data->allocatorObj ) {
 		data->allocatorObj->deallocate( this->_trusted_memory, data->memSize );
@@ -23,104 +40,88 @@ allocator_buddies_system::~allocator_buddies_system() {
 		::operator delete( this->_trusted_memory );
 	}
 }
-allocator_buddies_system::allocator_buddies_system(
-        size_t size, std::pmr::memory_resource *parentAllocator,
-        logger *logger, allocator_with_fit_mode::fit_mode fitMode) {
+allocator_buddies_system::allocator_buddies_system( size_t size, std::pmr::memory_resource* parentAllocator, logger* logger, allocator_with_fit_mode::fit_mode fitMode ) {
+	if( size < 5 ) throw std::logic_error("[BUDDY] Insufficient space");
 	
-	if( size < 5 ) {
-		throw std::logic_error("[BUDDY] Insufficient space");
-	}
-	
-	size_t allocSize = (1 << (int)(log2( size ))) + sizeof(struct BuddyAllocatorMetadata) + sizeof(struct BuddyAllocatorBlockMetadata);
-	
-	
+	size_t allocSize = (1 << (size_t)(log2( size ))) + sizeof(BuddyMetadata);
 	
 	this->_trusted_memory = (parentAllocator == nullptr) ? ::operator new(allocSize) : parentAllocator->allocate(allocSize, 1);
 	
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	BuddyMetadata* data = (BuddyMetadata*)this->_trusted_memory;
 	
-	data->loggerObj = logger ? logger : nullptr;
-	data->allocatorObj = parentAllocator ? parentAllocator : nullptr;
+	memset(&data->blocks, 0, sizeof(BuddyMetadata));
+	data->loggerObj = logger;
+	data->allocatorObj = parentAllocator;
 	data->fitMode = fitMode;
 	data->memSize = allocSize;
-	data->begin = reinterpret_cast<struct BuddyAllocatorBlockMetadata*>(reinterpret_cast<char*>(this->_trusted_memory) + sizeof( struct BuddyAllocatorMetadata ));
 	
-	data->begin->occupied = false;
-	data->begin->size = log2(size);
+	data->blocks[(size_t)(log2(size))] = (allocator_buddies_system::BuddyBlock*)((uintptr_t)this->_trusted_memory + sizeof(BuddyMetadata));
+	
+	data->blocks[(size_t)(log2(size))]->occupied = false;
+	data->blocks[(size_t)(log2(size))]->size = log2(size);
+	data->blocks[(size_t)(log2(size))]->next = 0;
 }
 
 [[nodiscard]] void *allocator_buddies_system::do_allocate_sm( size_t size ) {
     std::lock_guard lock(this->mutex());
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
-	size_t realSize = size; // + pow(2, 5); // 5 - log2(sizeof(struct BuddyAllocatorBlockMetadata))
-	debug_with_guard("[BUDDY] Allocating " + std::to_string(realSize) + " bytes");
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
+	debug_with_guard("[BUDDY] Allocating " + std::to_string(size) + " bytes");
 	
-	struct BuddyAllocatorBlockMetadata* freeBlock = nullptr;
-	
-	switch( data->fitMode ) {
-        case allocator_with_fit_mode::fit_mode::first_fit:
-            freeBlock = get_first(realSize);
-            break;
-        case allocator_with_fit_mode::fit_mode::the_best_fit:
-            freeBlock = get_best(realSize);
-            break;
-        case allocator_with_fit_mode::fit_mode::the_worst_fit:
-            freeBlock = get_worst(realSize);
-            break;
-		default:
-			throw std::bad_alloc();
-	}
+	allocator_buddies_system::BuddyBlock* freeBlock = this->get_block(size);
 	
 	if( freeBlock == nullptr ) {
-		debug_with_guard("[BUDDY] Unable to allocate " + std::to_string(realSize) + " bytes");
+		debug_with_guard("[BUDDY] Unable to allocate " + std::to_string(size) + " bytes");
         return nullptr;
 	}
 	
 	debug_with_guard("[BUDDY] Block found." + std::to_string((size_t)freeBlock) );
 	
-	while( pow(2, freeBlock->size) > realSize * 2 && freeBlock->size > 4 ) { // 4 is required by tests
-		freeBlock->size--;
-		struct BuddyAllocatorBlockMetadata* next = this->next_block(freeBlock);
-		next->size = freeBlock->size;
-		next->occupied = false;
-		// if( next ) next->prev = realNext;
-	}
-	debug_with_guard("[BUDDY] Splitting process finished." );
-	
-	if( freeBlock->size != realSize ) {
+	if( freeBlock->size != size ) {
 		warning_with_guard( "[BUDDY] allocated space of " + std::to_string(pow(2,freeBlock->size)) + "bytes allocated instead of requested space!" );
 	}
 
-	freeBlock->occupied = true;
 	
-	return reinterpret_cast<void*>(reinterpret_cast<std::byte*>(freeBlock) + sizeof(struct BuddyAllocatorBlockMetadata));
+	return reinterpret_cast<void*>((uintptr_t)(freeBlock) + 1);
 }
 
 
 void allocator_buddies_system::do_deallocate_sm( void *at ) {
 	if( at == nullptr ) return;
     std::lock_guard lock(this->mutex());
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
-	struct BuddyAllocatorBlockMetadata* block = reinterpret_cast<struct BuddyAllocatorBlockMetadata*>(reinterpret_cast<std::byte*>(at) - sizeof(struct BuddyAllocatorBlockMetadata));
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
+	allocator_buddies_system::BuddyBlock* block = (allocator_buddies_system::BuddyBlock*)((uintptr_t)(at) - 1);
 	
-	if( reinterpret_cast<std::byte*>(at) < reinterpret_cast<std::byte*>(this->_trusted_memory) || reinterpret_cast<std::byte*>(at) > reinterpret_cast<std::byte*>(this->_trusted_memory) + data->memSize + sizeof(struct BuddyAllocatorBlockMetadata) ) {
+	debug_with_guard("[BUDDY] Freeing obj");
+	if( (uintptr_t)(at) < (uintptr_t)(this->_trusted_memory) || (uintptr_t)(at) > (uintptr_t)(this->_trusted_memory) + data->memSize + sizeof(allocator_buddies_system::BuddyBlock) ) {
 		error_with_guard("[BUDDY] Invalid deallocation");
 		throw std::logic_error("[BUDDY] Invalid deallocation!");
 	}
 	
-	// while( block->prev && block->prev->occupied && block->prev->size == block->size )
-		// block = block->prev;
-	
-	
-	while( this->next_block(block) && !this->next_block(block)->occupied && this->next_block(block)->size == block->size ) {
+	while( this->get_buddy(block) && !this->get_buddy(block)->occupied && this->get_buddy(block)->size == block->size ) {
+		if( this->get_buddy(block) < block ) block = this->get_buddy(block);
+		
+		allocator_buddies_system::BuddyBlock* buddy = this->get_buddy(block);
+		
+		if( buddy->prev == 0 ) {
+			if( buddy->next ) reinterpret_cast<allocator_buddies_system::BuddyBlock*>(buddy->next)->prev = 0;
+			data->blocks[buddy->size] = reinterpret_cast<allocator_buddies_system::BuddyBlock*>(buddy->next);
+		}else{
+			reinterpret_cast<allocator_buddies_system::BuddyBlock*>(buddy->prev)->next = buddy->next;
+			if( buddy->next ) reinterpret_cast<allocator_buddies_system::BuddyBlock*>(buddy->next)->prev = buddy->prev;
+		}
+		
 		block->size++;
 		debug_with_guard("[BUDDY] Merged Block!");
-		// this->next_block(block) = this->next_block(this->next_block(block));
-		// if( this->next_block(block) ) this->next_block(block)->prev = block;	
 	}
 	
-	block->occupied = false;
+	if( data->blocks[block->size] != nullptr ) {
+		block->next = (uintptr_t)data->blocks[block->size];
+		data->blocks[block->size]->prev = (uintptr_t)block;
+	}
+	data->blocks[block->size] = block;
+	block->prev = 0;
 	
+	block->occupied = false;
 }
 
 allocator_buddies_system::allocator_buddies_system(const allocator_buddies_system &other) {
@@ -139,12 +140,12 @@ bool allocator_buddies_system::do_is_equal(const std::pmr::memory_resource &othe
 }
 
 logger* allocator_buddies_system::get_logger() const {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
     return data->loggerObj;
 }
 
 inline void allocator_buddies_system::set_fit_mode( allocator_with_fit_mode::fit_mode mode ) {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
     std::lock_guard lock(this->mutex());
     data->fitMode = mode;
 }
@@ -161,64 +162,67 @@ inline std::string allocator_buddies_system::get_typename() const {
 }
 
 std::vector<allocator_test_utils::block_info> allocator_buddies_system::get_blocks_info_inner() const {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
     std::vector<allocator_test_utils::block_info> out;
 
     std::back_insert_iterator<std::vector<allocator_test_utils::block_info>> inserter(out);
 
-	struct BuddyAllocatorBlockMetadata* curBlock = data->begin;
+	allocator_buddies_system::BuddyBlock* curBlock = reinterpret_cast<allocator_buddies_system::BuddyBlock*>((uintptr_t)data + sizeof(BuddyMetadata));
 	
-	struct BuddyAllocatorBlockMetadata* worstBlock = nullptr;
+	allocator_buddies_system::BuddyBlock* worstBlock = nullptr;
 	do {
 		inserter = {static_cast<size_t>(pow(2, curBlock->size)), curBlock->occupied};
-	} while( (curBlock=this->next_block(curBlock)) != nullptr );
+	} while( (curBlock=this->next_block(curBlock)) != nullptr && curBlock->size != 0 );
 
     return out;
 }
 
-struct BuddyAllocatorBlockMetadata* allocator_buddies_system::get_best(size_t size) noexcept {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
-	struct BuddyAllocatorBlockMetadata* best = nullptr;
+void allocator_buddies_system::split_first_block( allocator_buddies_system::BuddyBlock* block ) {
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
+	size_t oldSize = block->size;
+	data->blocks[oldSize] = reinterpret_cast<allocator_buddies_system::BuddyBlock*>(block->next);
 	
-	struct BuddyAllocatorBlockMetadata* cur = data->begin;
-	uint8_t bestSize = ~uint8_t(0);
 	
-	do {
-		if( !cur->occupied && pow(2, cur->size) >= size && cur->size < bestSize ) {
-			bestSize = cur->size;
-			best = cur;
-		}
-	} while( (cur=this->next_block(cur)) != nullptr );
+	block->size--;
+	
+	allocator_buddies_system::BuddyBlock* buddy = this->get_buddy( block );
 
-    return best;
+	buddy->next = (uintptr_t)data->blocks[block->size];
+	if( buddy->next != 0 ) (reinterpret_cast<allocator_buddies_system::BuddyBlock*>(buddy->next))->prev = (uintptr_t)buddy;
+	block->next = (uintptr_t)buddy;
+	buddy->prev = (uintptr_t)block;
+	block->prev = 0;
+	
+	buddy->size = block->size;
+	buddy->occupied = false;
+	
+	data->blocks[block->size] = block;
 }
 
-struct BuddyAllocatorBlockMetadata* allocator_buddies_system::get_worst(size_t size) noexcept {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
-	struct BuddyAllocatorBlockMetadata* worst = nullptr;
+allocator_buddies_system::BuddyBlock* allocator_buddies_system::get_block(size_t size) noexcept {
+	BuddyMetadata* data = (BuddyMetadata*)(this->_trusted_memory);
 	
-	struct BuddyAllocatorBlockMetadata* cur = data->begin;
-	uint8_t worstSize = 0;
+	allocator_buddies_system::BuddyBlock** blocks = data->blocks;
 	
-	do {
-		if( !cur->occupied && pow(2, cur->size) >= size && cur->size > worstSize ) {
-			worstSize = cur->size;
-			worst = cur;
+	size_t initialOffset = size == 0 ? 4 : ceil(log2(size));
+	if( size == (size_t)pow(2, initialOffset) ) initialOffset++;
+	for( size_t offset = initialOffset; offset < CPU_VM_BITS; ++offset ) {
+		if( blocks[offset] && blocks[offset]->size != offset ) return nullptr;
+		if( blocks[offset] ) {
+			
+			while( initialOffset < offset ) {
+				this->split_first_block( blocks[offset--] );
+			}
+			
+			blocks[offset]->occupied = true;
+			blocks[offset]->size = offset;
+			allocator_buddies_system::BuddyBlock* block = blocks[offset];
+			blocks[offset] = reinterpret_cast<allocator_buddies_system::BuddyBlock*>(blocks[offset]->next);
+			if( blocks[offset] ) blocks[offset]->prev = 0;
+			
+			return block;
 		}
-	} while( (cur=this->next_block(cur)) != nullptr );
-
-    return worst;
-}
-
-
-struct BuddyAllocatorBlockMetadata* allocator_buddies_system::get_first(size_t size) noexcept {
-	struct BuddyAllocatorMetadata* data = reinterpret_cast<struct BuddyAllocatorMetadata*>(this->_trusted_memory);
+	}
 	
-	struct BuddyAllocatorBlockMetadata* cur = data->begin;
-	
-	do {
-		if( !cur->occupied && pow(2, cur->size) > size ) return cur;
-	} while( (cur=this->next_block(cur)) != nullptr );
-
-    return nullptr;
+	return nullptr;
 }
